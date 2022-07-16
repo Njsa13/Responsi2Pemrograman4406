2792401864b4b7977d985a37dd421a28bff50e70
Najib Sauqi R <97015365+Njsa13@users.noreply.github.com>
2022-07-16 10:27:03 +0700

    Tambahkan Cs

---
+++ Mahasiswa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    public class Mahasiswa
+    {
+        // PERINTAH: lengkapi property class mahasiswa, sesuai petunjuk soal
+        public string Nim { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public double Ipk { get; set; }
+    }
+}
